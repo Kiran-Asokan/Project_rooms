5b9ce7efa0899fb531f7c7fc6a8a971b56726ed9
Kiran Asokan <kiranasokan101@gmail.com>
2024-08-11 18:15:23 +0530

    empty user message

--- Rooms_frontend_UI/roomsui/src/Components/ListingUsers.js
+++ Rooms_frontend_UI/roomsui/src/Components/ListingUsers.js
@@ -3,11 +3,18 @@ import ListingComponent from './ListingComponent'
 const ListingUser = (props) => {
     return (
             <div className='listinguser'>
-                {props?.users.map((user) => {
-                    return (<div onClick={ () =>  props.fetchBills(user.id)} key={user.id} className = 'user_comp' >
-                        <ListingComponent handleVacate = {props.handleVacate} fetchBills = {props.fetchBills} user={user} />
-                    </div>)
-                })}
+                {props.users.length !== 0 ? (
+                    props?.users.map((user) => {
+                        return (<div onClick={ () =>  props.fetchBills(user.id)} key={user.id} className = 'user_comp' >
+                            <ListingComponent handleVacate = {props.handleVacate} fetchBills = {props.fetchBills} user={user} />
+                        </div>)
+                    })
+                ) : 
+                <div>
+                    No active Users
+                </div> 
+                }
+                {}
             </div>
             
 
